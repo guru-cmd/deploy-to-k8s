d6c6cc69869a8653c2b022bfca44fa072dc57ae6
Gurudatta <gpanda1989@gmail.com>
2021-11-08 16:22:15 +0000

    chages in deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #get hash values from recent commit
-GIT_SHA= $(git rev-parse HEAD)
+GIT_SHA=$(git rev-parse HEAD)
 
 #build the docker image with recent git comit hash
-doker build -t gur3344/static-website-k8s:$GIT_SHA .
+docker build -t guru3344/static-website-k8s:$GIT_SHA .
 
 #push the image to docker hub
-docker push gur3344/static-website-k8s:$GIT_SHA
+#docker login
+docker push guru3344/static-website-k8s:$GIT_SHA
 
 #Apply the K8S objects
-kubctl apply -f k8s/namespace.yaml
-kubctl apply -f k8s/
+kubectl apply -f k8s/namespace.yaml
+kubectl apply -f k8s/
 
 #update the image in deployment resource/deployment yaml file
-kubctl set image deployment.apps/static-web-deployment -n=sample days-app-container=gur3344/static-website-k8s:$GIT_SHA
+kubectl set image deployment.apps/static-web-deployment -n=sample days-app-container=gur3344/static-website-k8s:$GIT_SHA
